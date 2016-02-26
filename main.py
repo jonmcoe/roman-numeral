5a928fb35161b2bccff1957ec2e18684e90d9021
Jon Coe <jonathon.coe@offerpop.com>
2016-02-25 22:19:24 -0500

    parameterized tests

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sys
 
 from roman_numeral import convert
-from tests import RomanNumeralTestCase
 
-test_case = RomanNumeralTestCase(debug=True)
-test_case.runTest()
 
 print(convert(int(sys.argv[1])))
--- tests.py
+++ tests.py
@@ -1,41 +1,38 @@
 from unittest import TestCase
 
+from nose_parameterized import parameterized
+
 from roman_numeral import convert
 
 
-cases = {
-    1: 'I',
-    3: 'III',
-    4: 'IV',
-    5: 'V',
-    7: 'VII',
-    10: 'X',
-    11: 'XI',
-    14: 'XIV',
-    15: 'XV',
-    19: 'XIX',
-    50: 'L',
-    83: 'LXXXIII',
-    100: 'C',
-    500: 'D',
-    1000: 'M',
-    1001: 'MI',
-    1900: 'MCM',
-    1987: 'MCMLXXXVII',
-    50005: 50*'M'+'V',
-}
+TEST_PARAMS = [
+    (1, 'I'),
+    (3, 'III'),
+    (4, 'IV'),
+    (5, 'V'),
+    (7, 'VII'),
+    (10, 'X'),
+    (11, 'XI'),
+    (14, 'XIV'),
+    (15, 'XV'),
+    (19, 'XIX'),
+    (50, 'L'),
+    (100, 'C'),
+    (83, 'LXXXIII'),
+    (500, 'D'),
+    (1000, 'M'),
+    (1001, 'MI'),
+    (1900, 'MCM'),
+    (1987, 'MCMLXXXVII'),
+    (50005, 50*'M' + 'V'),
+]
 
 
 class RomanNumeralTestCase(TestCase):
-    pass
 
-
-def make_function(decimal_input, roman_output):
-    def _current(self):
-        self.assertEquals(convert(decimal_input), roman_output)
-    _current.__name__ = 'test_'+str(decimal_input)
-    return _current
-
-for k, v in cases.items():
-    f = make_function(k, v)
-    setattr(RomanNumeralTestCase, f.__name__, f)
+    @parameterized.expand(
+        TEST_PARAMS,
+        testcase_func_name=lambda func_name, _, params: 'test_{0}_is_{1}'.format(*params.args)
+    )
+    def test_(self, decimal, roman):
+        self.assertEquals(convert(int(decimal)), roman)

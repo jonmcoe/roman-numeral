12a9d0f3f6427217c33d7d1f47a941edacd3d56b
Jon Coe <jonathon.coe@offerpop.com>
2016-02-28 22:40:45 -0500

    lambda_function

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+from roman_numeral import convert_to_decimal, convert_to_roman
+# ---
+
+import logging
+
+logger = logging.getLogger()
+
+
+def lambda_handler(event, context, *args, **kwargs):
+    if not event.get('value'):
+        return None
+
+    input_value = event['value']
+    try:
+        to_roman = not input_value.isalpha()
+
+        initial_chunks = input_value.split(' ') # must have spaces between operators
+        formatter = lambda x: str(convert_to_decimal(x.strip())) if x.isalpha() else x
+        chunks = map(formatter, initial_chunks)
+        final_string = ''.join(chunks)
+        ans = eval(final_string)
+        return convert_to_roman(ans) if to_roman else ans
+    except Exception:
+        logger.exception("")
+        return "Bad format"

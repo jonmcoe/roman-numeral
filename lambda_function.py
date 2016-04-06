d39a25dd886ad40236b096689d0a70e66569fcd0
Jon Coe <jonathon.coe@offerpop.com>
2016-04-05 21:09:26 -0400

    more logging, comments in lambda

--- lambda_function.py
+++ lambda_function.py
@@ -15,12 +15,17 @@ def lambda_handler(event, context, *args, **kwargs):
     try:
         to_roman = not input_value.isalpha()
 
+        # extremely half-ass (quarter-ass?) parsing
         initial_chunks = input_value.split(' ') # must have spaces between operators
         formatter = lambda x: str(convert_to_decimal(x.strip())) if x.isalpha() else x
         chunks = map(formatter, initial_chunks)
         final_string = ''.join(chunks)
-        ans = eval(final_string)
+        ans = eval(final_string)  # unsafe? sure. but it's ok bc its an AWS lambda
+
         return convert_to_roman(ans) if to_roman else ans
     except Exception:
+        if 'final_string' not in locals():
+            final_string = 'unreachable'
+        logger.error("Original: {0}, Translated: {1}".format(input_value, final_string))
         logger.exception("")
         return "Bad format"

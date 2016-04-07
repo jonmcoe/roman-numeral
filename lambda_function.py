6b160c6bf2edd641ca790eca3955cc970281d056
Jon Coe <jonathon.coe@offerpop.com>
2016-04-06 22:55:35 -0400

    structured json responses

--- lambda_function.py
+++ lambda_function.py
@@ -12,10 +12,6 @@ def lambda_handler(event, context, *args, **kwargs):
 
     input_value = event['value']
     input_value = input_value if isinstance(input_value, basestring) else str(input_value)
-    # default output is roman string. it is decimal iff either:
-    #   1) explicitly specified in POST body's `output_to_decimal` field
-    #   2) or input is pure alphabetic (otherwise roman output would be identical to input, which seems undesirable)
-    output_to_decimal = event.get('output_to_decimal', input_value.isalpha())
 
     try:
         # extremely half-ass (quarter-ass?) parsing
@@ -25,9 +21,14 @@ def lambda_handler(event, context, *args, **kwargs):
         final_string = ''.join(chunks)
         ans = eval(final_string)  # unsafe? sure. but it's ok bc its an AWS lambda
 
-        return ans if output_to_decimal else convert_to_roman(ans)
+        return {
+            "roman": convert_to_roman(ans),
+            "decimal": ans
+        }
     except Exception:
         logger.exception("Original: {0}, Translated: {1}".format(
             input_value, locals().get('final_string', 'unreachable')
         ))
-        return "Bad format"
+        return {
+            "errorMessage": "Bad format"
+        }

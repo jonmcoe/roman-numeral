64ab6872adad575ff66c2ce5adf7341029131595
Jon Coe <jonathon.coe@offerpop.com>
2016-04-15 11:43:52 -0400

    required arguments for lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,31 @@
 from roman_numeral import convert_to_decimal, convert_to_roman
 # ---
-
+import json
 import logging
 
 logger = logging.getLogger()
 
 
+class MissingRequiredParameterException(Exception):
+
+    def __init__(self, missing_params):
+        self.message = "Missing required parameters: {0}".format(', '.join(list(missing_params)))
+        super(MissingRequiredParameterException, self).__init__()
+
+
 def lambda_handler(event, context):
     context = None  # we don't use it, and don't want to potentially expose in the eval below
-    if not event.get('value'):
-        return None
-
-    input_value = event['value']
-    input_value = input_value if isinstance(input_value, basestring) else str(input_value)
+    REQUIRED_PARAMS = {'value'}
+    missing_params = REQUIRED_PARAMS - set(event.keys())
 
     try:
+        if missing_params:
+            input_value = json.dumps(event)
+            raise MissingRequiredParameterException(missing_params)
+
+        input_value = event['value']
+        input_value = input_value if isinstance(input_value, basestring) else str(input_value)
+
         # extremely half-ass (quarter-ass?) parsing
         initial_chunks = input_value.split(' ')  # must have spaces between operators and roman characters
         formatter = lambda x: str(convert_to_decimal(x.strip())) if x.isalpha() else x
@@ -30,4 +41,4 @@ def lambda_handler(event, context):
         logger.exception("Original: {0}, Translated: {1}".format(
             input_value, locals().get('final_string', 'unreachable')
         ))
-        raise e.__class__("Bad Request: " + e.__class__.__name__ + ' w/ ' + e.message)
+        raise Exception("Bad Request: " + e.__class__.__name__ + ' w/ ' + e.message)
